76739687abbf5e5d7f160e0faa4c86f49250ab01
Thezx33 <oscarmiguelayalaruelas@gmail.com>
2020-03-19 18:55:45 -0700

    Comentarios en Dart + documentacion

---
+++ 04_comentarios.dart
@@ -0,0 +1,23 @@
+main(){
+
+    // Esta es una variable temporal que quiero definir | Comentario en línea
+    int a = 10; // a debería de ser 10
+
+    /* | Comentario en bloque
+    final personas = [
+        // Esta es la primer persona
+        'Fernando', // [0]
+
+        // Esta es la segunda perosna
+        'Pedro', // [1]
+
+        // Esta es la tercera persona
+        'Carlos' // [2]
+
+    ];
+    */
+}
+
+/// Funciona para saludar
+/// Recibe un [nombre] y se concatena con el [mensaje]
+saludar( int nombre, int mensaje ){}
